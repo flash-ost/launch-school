d2e737bb211dd131ce273f8d6541237484119921
flash-ost <dotoner555@gmail.com>
2024-06-04 22:20:37 -0700

    Allow shortened names as input

--- py101/lesson2/rock_paper_scissors.py
+++ py101/lesson2/rock_paper_scissors.py
@@ -1,11 +1,26 @@
 from random import choice
 from os import system
 
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+VALID_CHOICES = {
+    'r': 'rock',
+    'p': 'paper',
+    'sc': 'scissors',
+    'l': 'lizard',
+    'sp': 'spock',
+}
 VALID_ANSWERS = ['n', 'no', 'y', 'yes']
+WINNING_COMBOS = {
+    'rock':     ['scissors', 'lizard'],
+    'paper':    ['rock',     'spock'],
+    'scissors': ['paper',    'lizard'],
+    'lizard':   ['paper',    'spock'],
+    'spock':    ['rock',     'scissors'],
+}
+WINS_LIMIT = 3
 
 # Greet user and explain rules
 def greet_user():
+    system('clear')
     prompt('Welcome to game Rock, Paper, Scissors, Lizard, Spock!')
     prompt("""The rules are (not so) simple:
            Scissors cut Paper
@@ -18,65 +33,78 @@ def greet_user():
            Paper disproves Spock
            Spock vaporizes Rock
            Rock crushes Scissors""")
-    prompt('The game is best of 5, ties don\'t count. Good luck!')
-    prompt('------------------------------------------------')
+    prompt('The game is played to 3 wins, ties don\'t count. Good luck!')
+    prompt('----------------------------------------------------------')
 
 # Print a formatted message
 def prompt(message):
     print(f"==> {message}")
-    
+
 # Ask user to make a choice
 def prompt_input():
-    prompt('Choose one: ' + ', '.join(VALID_CHOICES))
-    choice = input()
-    while choice not in VALID_CHOICES:
-        prompt('That\'s not a valid choice')
-        choice = input()
-    return choice
+    prompt('Choose one: ' + ', '.join(VALID_CHOICES.values()))
+    prompt('You can also type abbreviated names: ' +
+           ', '.join(VALID_CHOICES.keys()))
+    user_choice = input().lower()
 
+    # Ensure input is valid
+    while user_choice not in VALID_CHOICES \
+    and user_choice not in VALID_CHOICES.values():
+        prompt('That\'s not a valid choice')
+        user_choice = input()
+
+    # Ensure full name is stored
+    if len(user_choice) <= 2:
+        user_choice = VALID_CHOICES[user_choice]
+    return user_choice
+
+# Host a round
 def host_round():
-    global round_count
     prompt(f'Round {round_count}')
-    round_count += 1
+
+    # Gather input from user and computer
     user_choice = prompt_input()
+    computer_choice = choice(tuple(VALID_CHOICES.values()))
+
+    # Display winner and score
     system('clear')
-    computer_choice = choice(VALID_CHOICES)
     round_winner = determine_winner(user_choice, computer_choice)
+    if round_winner:
+        scoreboard[round_winner] += 1
     display_round_result(user_choice, computer_choice, round_winner)
 
-def determine_winner(user, computer):
-    if user == computer:
+# Determine winner (or tie)
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
         return False
-    elif ((user == 'rock' and computer in ['lizard', 'scissors'])
-        or (user == 'paper' and computer in ['rock', 'spock'])
-        or (user == 'scissors' and computer in ['paper', 'lizard'])
-        or (user == 'lizard' and computer in ['spock', 'paper'])
-        or (user == 'spock' and computer in ['scissors', 'rock'])):
-        scoreboard['user'] += 1
-        return 'user'
-    else:
-        scoreboard['computer'] += 1
-        return 'computer'
+    return 'user' if computer_choice in WINNING_COMBOS[user_choice] \
+        else 'computer'
 
 # Display round result
 def display_round_result(user_choice, computer_choice, winner):
+    prompt(f'Round {round_count} results')
     prompt(f'You chose {user_choice}, computer chose {computer_choice}')
     match winner:
         case 'user':
             prompt('You win!')
         case 'computer':
             prompt('Computer wins!')
-        case _:
+        case False:
             prompt('It\'s a tie!')
-    prompt(f"""USER [{scoreboard['user']}] : [{scoreboard['computer']}] COMPUTER""")
+    prompt(f'USER [{scoreboard['user']}] : '
+           f'[{scoreboard['computer']}] COMPUTER')
     prompt('------------------------------------------------')
 
 # Display game result
 def display_game_result():
     if scoreboard['user'] == 3:
-        prompt(f'You won a game after {round_count} rounds with score {scoreboard['user']}:{scoreboard['computer']}. Congratulations! Did Sheldon train you?')
+        prompt(f'You won a game after {round_count} rounds with score '
+               f'{scoreboard['user']}:{scoreboard['computer']}. '
+               'Congratulations! Did Sheldon train you?')
     else:
-        prompt(f'Computer won a game after {round_count} rounds with score {scoreboard['computer']}:{scoreboard['user']}. Better luck next time!')
+        prompt(f'Computer won a game after {round_count} rounds with score '
+               f'{scoreboard['computer']}:{scoreboard['user']}. '
+               'Better luck next time!')
 
 # Ask user for another game
 def another_game():
@@ -88,16 +116,19 @@ def another_game():
             answer = input()
         else:
             break
-    return answer.lower() in ['y', 'yes']       
+    return answer.lower() in ['y', 'yes']
 
+# Main program
 greet_user()
 while True:
-    round_count = 1
+    round_count = 0
     scoreboard = {'user': 0, 'computer': 0}
-    while scoreboard['user'] < 3 and scoreboard['computer'] < 3:
+    while scoreboard['user'] < WINS_LIMIT \
+    and scoreboard['computer'] < WINS_LIMIT:
+        round_count += 1
         host_round()
-    display_game_result()    
-    
+    display_game_result()
+
     if not another_game():
         break
     system('clear')
