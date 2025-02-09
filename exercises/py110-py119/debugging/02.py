055adb5d7228d25a7a7f70f9e7ef6ca935da70f2
flash-ost <dotoner555@gmail.com>
2025-02-09 12:20:00 -0800

    Minor change to ex.2

--- exercises/py110-py119/debugging/02.py
+++ exercises/py110-py119/debugging/02.py
@@ -11,12 +11,11 @@ def reverse_string(string):
 print(reverse_string("hello") == "olleh")
 """
 
+# Inside a function we iterate over original string. Inside a loop body we reassign a string to a new value:
+# Current character + the string itself. So after first iteration string 'hello' looks like this: 'hhello'.
+# The simplest way to fix the code would be to use Python's slicing to reverse the string:
+
 def reverse_string(string):
     return string[::-1]
 
 print(reverse_string("hello") == "olleh")
-
-# Inside a function we iterate over original string. Inside a loop body we reassign a string to a new value:
-# Current character + the string itself. So after first iteration string 'hello' looks like this: 'hhello'.
-# After loop finishes iterating over original string, resulting string looks like this: 'ollehhello'.
-# The simplest way to fix the code would be to use Python's slicing to reverse the string:
