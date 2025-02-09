81c49f07e6297c2367779da97c653bd7e92ee81c
flash-ost <dotoner555@gmail.com>
2025-02-09 12:17:29 -0800

    Debugging, ex. 1, 2

---
+++ exercises/py110-py119/debugging/01.py
@@ -0,0 +1,19 @@
+"""
+Countdown
+Our countdown to launch isn't behaving as expected. Why? Change the code so that our program successfully counts down from 10 to 1 before launching.
+
+def decrease(counter):
+    return counter - 1
+
+counter = 10
+
+for _ in range(10):
+    print(counter)
+    decrease(counter)
+
+print('LAUNCH!')
+"""
+for counter in range(10, 0, -1):
+    print(counter)
+
+print('LAUNCH!')
---
+++ exercises/py110-py119/debugging/02.py
@@ -0,0 +1,22 @@
+"""
+Reverse a String
+You have a function that is supposed to reverse a string passed as an argument. However, it's not producing the expected output. Explain the bug, and provide a solution.
+
+def reverse_string(string):
+    for char in string:
+        string = char + string
+
+    return string
+
+print(reverse_string("hello") == "olleh")
+"""
+
+def reverse_string(string):
+    return string[::-1]
+
+print(reverse_string("hello") == "olleh")
+
+# Inside a function we iterate over original string. Inside a loop body we reassign a string to a new value:
+# Current character + the string itself. So after first iteration string 'hello' looks like this: 'hhello'.
+# After loop finishes iterating over original string, resulting string looks like this: 'ollehhello'.
+# The simplest way to fix the code would be to use Python's slicing to reverse the string:
