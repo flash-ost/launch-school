289826b272c955b47585203402c9f9920c3ec1c3
flash-ost <dotoner555@gmail.com>
2025-12-08 22:53:35 -0800

    Various improvements

--- py175/lesson3/users_and_interests/app.py
+++ py175/lesson3/users_and_interests/app.py
@@ -3,11 +3,15 @@ import yaml
 
 app = Flask(__name__)
 
+def calculate_statistics():
+    return {'users': len(g.data),
+            'interests': sum([len(g.data[user]['interests']) for user in g.data])}
+
 @app.before_request
 def load_data():
     with open('users.yaml', 'r') as file:
         g.data = yaml.safe_load(file)
-    g.count = count()
+    g.count = calculate_statistics()
 
 @app.template_filter('others_lined')
 def others_lined(others):
@@ -15,29 +19,35 @@ def others_lined(others):
              for other in others]
     return ' | '.join(links)
 
+@app.route('/')
+def home():
+    return redirect(url_for('users'))
+
 @app.route('/users')
 def users():
-    names = [user for user in g.data.keys()]
-    return render_template('users.html', names=names, count=g.count)
+    return render_template('users.html', names=g.data.keys(), count=g.count)
 
 @app.route('/<name>')
 def user(name):
-    data = g.data[name]
-    others = [user for user in g.data.keys()]
-    others.remove(name)
-    return render_template('user.html',
-                           name=name,
-                           data=data,
-                           others=others,
-                           count=g.count)
+    try:
+        data = g.data[name]
+        others = [user for user in g.data.keys() if user != name]
+        return render_template('user.html',
+                               name=name,
+                               data=data,
+                               others=others,
+                               count=g.count)
+    except:
+        error_message = 'Sorry, there is no such user. ' \
+                        'Try one of the links at the bottom.'
+        return render_template('user.html',
+                               error_message=error_message,
+                               others = g.data.keys(),
+                               count=g.count)
 
 @app.errorhandler(404)
 def page_not_found(error):
     return redirect('/users')
-
-def count():
-    return {'users': len(g.data),
-            'interests': sum([len(g.data[user]['interests']) for user in g.data])}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
