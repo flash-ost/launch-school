4e5e0d360f9e1ebf6fd87a9aa6e0e7deb01a619f
flash-ost <dotoner555@gmail.com>
2025-12-20 20:25:05 -0800

    Minor fixes

--- py175/lesson4/todo/todos/utils.py
+++ py175/lesson4/todo/todos/utils.py
@@ -1,20 +1,15 @@
 def delete_todo_by_id(todo_id, lst):
     lst['todos'] = [todo for todo in lst['todos'] if todo['id'] != todo_id]
-    return None
 
 def error_for_list_title(title, lists):
     if any(lst['title'] == title for lst in lists):
         return 'The title must be unique.'
     elif not 1 <= len(title) <= 100:
         return 'The title must be between 1 and 100 characters.'
-    else:
-        return None
     
 def error_for_todo(todo):
     if not 1 <= len(todo) <= 100:
         return 'The title must be between 1 and 100 characters.'
-    else:
-        return None
 
 def find_list_by_id(list_id, lists):
     return next((lst for lst in lists if lst['id'] == list_id), None)
@@ -31,13 +26,9 @@ def is_todo_completed(todo):
 def mark_all_done(lst):
     for todo in lst['todos']:
         todo['completed'] = True
-    return None
 
 def sort_items(items, select_completed):
-    sorted_items = sorted(items, key=lambda lst: lst['title'].lower())
-    incomplete_items = [item for item in sorted_items if not select_completed(item)]
-    complete_items = [item for item in sorted_items if select_completed(item)]
-    return incomplete_items + complete_items
+    return sorted(items, key=lambda item: (select_completed(item), item['title'].lower()))
 
 def todos_remaining(lst):
     return sum(1 for todo in lst['todos'] if not todo['completed']) 
